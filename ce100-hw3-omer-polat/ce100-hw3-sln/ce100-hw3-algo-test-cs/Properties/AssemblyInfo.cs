3c2ac5fa54d08d98beb39ef818466fe70b010a07
omerpolat <omer_polat20@erdogan.edu.tr>
2022-04-10 21:40:20 +0300

    hw3

---
+++ ce100-hw3-omer-polat/ce100-hw3-sln/ce100-hw3-algo-test-cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ce100-hw3-algo-test-cs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ce100-hw3-algo-test-cs")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25d29b61-a286-40d8-a0ed-7bd5ef88ee7d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ce100-hw3-omer-polat/ce100-hw3-sln/ce100-hw3-algo-test-cs/UnitTest1.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ce100_hw3_sln;
+
+namespace ce100_hw3_algo_test_cs
+{
+
+    /****************************
+*****************************
+* @file ce100_hw3_algo_test_cs *
+* @author Ömer POLAT *
+* @date 10 April 2022 * 
+*
+* @brief <b> hw-3 Unit test </b> *
+*
+* 
+*
+* @see http://bilgisayar.mmf.erdogan.edu.tr/en/
+*
+*****************************
+*****************************/
+
+
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LongestCommonSubsquence_TestMethod1()
+        {
+            string s1 = "FMSMCŞSİDMÜQPWELQĞPFMCSAĞPDÖALSFMŞ";
+            string s2 = "MCŞSLMDĞAKEĞQŞLMASDFKSDİŞFMQMWĞEŞF";
+
+
+            int m = s1.Length;
+            int n = s2.Length;
+            string result = Class1.LongestCommonSubsquence(s1, s2, m, n);
+            string expected = "MCŞSMEĞMADSFMŞ";
+            Assert.AreEqual(result, expected);
+        }
+
+
+        [TestMethod]
+        public void LongestCommonSubsquence_TestMethod2()
+        {
+            string s1 = "RWEIRPWERIQĞPWEQĞPOQÜOR";
+            string s2 = "WEROĞLWEFWEÜFÖÜWLQĞÜWLÖ";
+
+
+            int m = s1.Length;
+            int n = s2.Length;
+            string result = Class1.LongestCommonSubsquence(s1, s2, m, n);
+            string expected = "WERWEWEQĞÜ";
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void LongestCommonSubsquence_TestMethod3()
+        {
+            string s1 = "FSNDLFKNSDFDCMSĞFLMASZ";
+            string s2 = "MCŞDSMDASŞFNCAŞSLDMŞLA";
+
+
+            int m = s1.Length;
+            int n = s2.Length;
+            string result = Class1.LongestCommonSubsquence(s1, s2, m, n);
+            string expected = "DSDFCSLMA";
+            Assert.AreEqual(result, expected);
+        }
+
+
+
+
+
+        [TestMethod]
+        public void MatrixChainOrder_TestMethod1()
+        {
+            int[] arr = { 1, 2, 3, 4 };
+            int n = arr.Length;
+            int res = Class1.MatrixChainOrder(arr, 1, n - 1);
+            Assert.AreEqual(res, 18);
+        }
+
+        [TestMethod]
+        public void MatrixChainOrder_TestMethod2()
+        {
+            int[] arr = new int[] { 1,2,3,4,3 };
+            int n = arr.Length;
+            int res = Class1.MatrixChainOrder(arr, 1, n - 1);
+            Assert.AreEqual(res, 30);
+        }
+
+        [TestMethod]
+        public void MatrixChainOrder_TestMethod3()
+        {
+            int[] arr = new int[] { 10, 20, 30, 40, 30 };
+            int n = arr.Length;
+            int res = Class1.MatrixChainOrder(arr, 1, n - 1);
+            Assert.AreEqual(res, 30000);
+        }
+
+
+
+
+
+       
+    }
+}
